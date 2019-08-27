07fbeb3e45949ec7da0c1bf5b99c8a9e7ad70eaf
Neeha Bollepalli <nbollepalli@pros.com>
2019-08-27 14:47:20 -0500

    Remove unnecessary import

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
